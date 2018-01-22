2dcd96e37a5f461528b886b57c82aad5ce8056a1
andrewmaz <andy1995>
2018-01-22 15:17:38 +0200

    final

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: amazurok <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 13:32:14 by amazurok          #+#    #+#             */
-/*   Updated: 2017/12/07 18:36:18 by amazurok         ###   ########.fr       */
+/*   Updated: 2017/12/11 18:22:35 by amazurok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-char	*ft_realloc(char *str, int size)
+
+char		*ft_realloc(char *str, int size)
 {
 	char *nstr;
 
@@ -20,13 +20,13 @@ char	*ft_realloc(char *str, int size)
 		return (NULL);
 	if (str)
 	{
-		ft_memcpy(nstr, str, size);
-		free(str);
+		ft_strcpy(nstr, str);
+		ft_strdel(&str);
 	}
 	return (nstr);
 }
 
-t_list *ft_add2list(t_list **list, size_t fd)
+t_list		*ft_add2list(t_list **list, size_t fd)
 {
 	t_list *lst;
 
@@ -34,8 +34,7 @@ t_list *ft_add2list(t_list **list, size_t fd)
 	if (!(*list))
 	{
 		*list = ft_lstnew("", fd);
-		free((*list)->content);
-		(*list)->content = NULL;
+		ft_memdel(&lst->content);
 		return (*list);
 	}
 	while (lst->next && lst->content_size != fd)
@@ -45,42 +44,42 @@ t_list *ft_add2list(t_list **list, size_t fd)
 	else
 	{
 		lst->next = ft_lstnew("", fd);
-		free((lst->next)->content);
-		(lst->next)->content = NULL;
+		ft_memdel(&lst->next->content);
 		return (lst->next);
 	}
 }
 
-char *ft_newline(char *str)
+char		*ft_newline(char *str)
 {
-	int i;
-	char *res;
+	int		i;
+	char	*res;
 
 	i = 0;
-	while(str[i] && str[i - 1] != '\n')
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
 		i++;
 	res = ft_strnew(ft_strlen(str + i));
 	res = ft_strcpy(res, str + i);
+	ft_strclr(str);
 	ft_strdel(&str);
 	return (res);
 }
 
-int ft_add2line(t_list *lst, char **line)
+int			ft_add2line(t_list *lst, char **line)
 {
 	int i;
 	int len;
 
 	i = 0;
-	if (*line)
-		len = ft_strlen(*line);
-	else
-		len = 0;
-	while (lst->content && ((char*)lst->content)[i] && ((char*)lst->content)[i] != '\n')
+	len = (*line) ? ft_strlen(*line) : 0;
+	while (lst->content && ((char*)lst->content)[i] && \
+		((char*)lst->content)[i] != '\n')
 		i++;
 	if (i > 0)
 	{
 		*line = ft_realloc(*line, len + i);
-		*line = ft_strcat(*line, ft_strsub(lst->content, 0, i));
+		*line = ft_strncat(*line, lst->content, i);
 	}
 	if (lst->content && ((char*)lst->content)[i] == '\n')
 	{
@@ -89,30 +88,26 @@ int ft_add2line(t_list *lst, char **line)
 		lst->content = ft_newline(lst->content);
 		return (1);
 	}
-	else
-	{
-		free(lst->content);
-		lst->content = NULL;
-		return (0);
-	}
+	ft_memdel(&lst->content);
+	return (0);
 }
 
-int get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
-	static t_list *head;
-	int ret;
-	char buf[BUFF_SIZE + 1];
-	int size;
-	t_list *qwe;
+	static t_list	*head;
+	int				ret;
+	char			buf[BUFF_SIZE + 1];
+	int				size;
+	t_list			*qwe;
 
-	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
+	if (fd < 0 || !line || read(fd, buf, 0) < 0 || BUFF_SIZE < 0)
 		return (-1);
 	size = 0;
 	*line = NULL;
 	qwe = ft_add2list(&head, (size_t)fd);
 	if (ft_add2line(qwe, line))
 		return (1);
-	while((ret = read(fd, buf, BUFF_SIZE)))
+	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
 		size += ret;
@@ -123,6 +118,5 @@ int get_next_line(const int fd, char **line)
 	}
 	if (*line && line[0])
 		return (1);
-	else
-		return (0);
+	return (0);
 }
